55a718eaf36d2b9d6ed7561dda3ec2b28c5fef72
Soe Yu Yu Kyaw <122962636+SoeYuYuKyaw@users.noreply.github.com>
2023-11-02 07:42:18 +0000

    update

--- src/main/java/th/mfu/domain/PlayerO.java
+++ src/main/java/th/mfu/domain/PlayerO.java
@@ -1,1 +1,33 @@
 package th.mfu.domain;
+
+import java.lang.annotation.Inherited;
+import java.util.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+
+public class PlayerO {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+
+    private String nameO;
+
+    public PlayerO(String nameO) {
+        this.nameO = nameO;
+    }
+
+    public String getNameO() {
+        return nameO;
+    }
+
+    public void setNameX(String nameO) {
+        this.nameO = nameO;
+    }
+
+}
+
+
