54865f52ec8930008d720584a006fd1afedc19f4
mttn <122962303+mtn94887@users.noreply.github.com>
2023-11-02 07:24:56 +0000

    naiani

---
+++ src/main/java/th/mfu/TictactoeController.java
@@ -0,0 +1,24 @@
+package th.mfu; 
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller 
+public class TictactoeController{
+
+}
