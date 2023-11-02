05da21af6a599d464f6639572a94b5e11ae4e45d
shwinpyonethu <122962983+shwinpyonethu@users.noreply.github.com>
2023-11-02 07:49:27 +0000

    update

--- src/main/java/th/mfu/domain/ScoresX.java
+++ src/main/java/th/mfu/domain/ScoresX.java
@@ -1,1 +1,28 @@
 package th.mfu.domain;
+
+import java.util.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+@Entity
+public class ScoresX {
+    
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int scoreX; 
+    private String nameX;
+    public int getScoreX() {
+        return scoreX;
+    }
+    public void setScoreX(int scoreX) {
+        this.scoreX = scoreX;
+    }
+    public String getNameX() {
+        return nameX;
+    }
+    public void setNameX(String nameX) {
+        this.nameX = nameX;
+    } 
+    
+}
