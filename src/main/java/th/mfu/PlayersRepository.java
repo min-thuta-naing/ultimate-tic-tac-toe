2f1441a365e997d28d542582232376454b232a8f
Phue Phue Soe Lwin <6531503170@lamduan.mfu.ac.th>
2023-11-12 13:32:13 +0700

    ppss

---
+++ src/main/java/th/mfu/PlayersRepository.java
@@ -0,0 +1,8 @@
+package th.mfu;
+
+import org.springframework.data.repository.CrudRepository;
+import th.mfu.domain.Players;
+
+public interface PlayersRepository extends CrudRepository<Players,Long>{
+    
+}
