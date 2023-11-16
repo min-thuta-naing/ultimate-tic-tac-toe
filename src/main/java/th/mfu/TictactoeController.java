bae2457b142d0ce4a30531480e0592262c856e53
Htet Yunn <6531503143@lamduan.mfu.ac.th>
2023-11-16 16:00:18 +0700

    hysy12345

--- src/main/java/th/mfu/TictactoeController.java
+++ src/main/java/th/mfu/TictactoeController.java
@@ -17,53 +17,39 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;  
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
 import th.mfu.domain.Players;
 import th.mfu.domain.Rounds;   
 @Controller
 public class TictactoeController {
     @Autowired
     private PlayersRepository playersRepository;
-
     public TictactoeController(PlayersRepository playersRepository){
     this.playersRepository = playersRepository;
     }
-
-
     //method for start page
     @GetMapping("/start-game")
     public String displayStartPage(Model model){
     return "start";
     }
-
-
     //method for accepting players's names (player name entry)
     @GetMapping("/name-entry")
     public String addNameForm(Model model){
     model.addAttribute("name", new Players());
     return "player-name-entry";
     }
-
-
     @PostMapping("/name-entry")
     public String saveNameX(@ModelAttribute Players name, Model model){
     playersRepository.save(name);
-
-
+    
     Iterable<Players> playersList = playersRepository.findAll();
     model.addAttribute("players", playersList);
     return "redirect:/name-entry";
     }
-
-
-
     //method for help page (help page)
     @GetMapping("/help")
     public String displayHelp(Model model){
     return "help";
     }
-
-
     //method for scoreboard page (listing)
     @GetMapping("/name-list")
     public String nameList(Model model){
@@ -73,8 +59,6 @@ public class TictactoeController {
         model.addAttribute("rounds", roundsList);
         return "list"; 
     }
-
-
     //method for displaying gameboard
     @GetMapping("/tictactoe")
     public String displayGameboard(Model model){
@@ -85,7 +69,6 @@ public class TictactoeController {
     
     @Autowired
     private RoundsRepository roundsRepository;
-
     @PostMapping("/save-result")
     public String saveGameResult(@RequestParam("winner") String winner, Model model) {
         if ("tie".equals(winner)) {
@@ -100,26 +83,15 @@ public class TictactoeController {
         }
         return "redirect:/name-list";
     }
-
-
-
-
-
-
-
         //method for renaming (updating)
-
-
-
-
         /*
         
-
         //method for renaming players(updating)
         /* @PostMapping("/rename")
         public String renameThePlayers (@ModelAttribute PlayerX nameX){
         repoX.updateNameById(nameX.getId(), nameX.getNameX());
         return "redirect:/name-list";
         } */
-
 }
+
+
