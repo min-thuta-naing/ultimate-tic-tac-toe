2fea0474e49270a327df5f78c39e8ecffabfd4b4
Min Thuta Naing <6531503157@lamduan.mfu.ac.th>
2023-11-23 13:19:14 +0700

    tt

--- src/main/java/th/mfu/App.java
+++ src/main/java/th/mfu/App.java
@@ -2,8 +2,12 @@ package th.mfu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "th.mfu.domain")
+@ComponentScan(basePackages = "th.mfu")
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
