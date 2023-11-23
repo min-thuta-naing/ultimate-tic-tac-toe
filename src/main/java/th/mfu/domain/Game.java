9f019bc67a3dcb6c8c16fafa5442010dd61c0c3c
Min Thuta Naing <6531503157@lamduan.mfu.ac.th>
2023-11-23 15:07:38 +0700

    fff

--- src/main/java/th/mfu/domain/Game.java
+++ src/main/java/th/mfu/domain/Game.java
@@ -2,8 +2,13 @@ package th.mfu.domain;
 
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity 
@@ -13,8 +18,18 @@ public class Game {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id; 
     private String gameName;
+
+     @OneToMany(mappedBy = "game", cascade = CascadeType.ALL)
+    private List<Players> players;
     
    
+    public List<Players> getPlayers() {
+        return players;
+    }
+    public void setPlayers(List<Players> players) {
+        this.players = players;
+    }
+    
     //constructor 
     public Game(){
     }
@@ -37,4 +52,6 @@ public class Game {
     public void setGameName(String gameName) {
         this.gameName = gameName;
     }
+
+
 }
