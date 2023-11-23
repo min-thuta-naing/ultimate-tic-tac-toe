6fc8dfe14ea755806b2bd971e1e22e77264defae
Min Thuta Naing <6531503157@lamduan.mfu.ac.th>
2023-11-23 13:46:36 +0700

    uu

--- src/main/java/th/mfu/ResetAutoIncrementService.java
+++ src/main/java/th/mfu/ResetAutoIncrementService.java
@@ -1,24 +1,24 @@
-package th.mfu;
+// package th.mfu;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.stereotype.Service;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.jdbc.core.JdbcTemplate;
+// import org.springframework.stereotype.Service;
 
-@Service
-public class ResetAutoIncrementService {
-    @Autowired
-    private JdbcTemplate jdbcTemplate;
+// @Service
+// public class ResetAutoIncrementService {
+//     @Autowired
+//     private JdbcTemplate jdbcTemplate;
 
-    public void resetAutoIncrement(String tableName) {
-        String resetSql = "ALTER TABLE " + tableName + " AUTO_INCREMENT = 1";
-        jdbcTemplate.execute(resetSql);
+//     public void resetAutoIncrement(String tableName) {
+//         String resetSql = "ALTER TABLE " + tableName + " AUTO_INCREMENT = 1";
+//         jdbcTemplate.execute(resetSql);
 
-        // Additional statement for InnoDB engine
-        String setEngineSql = "ALTER TABLE " + tableName + " ENGINE=InnoDB";
-        jdbcTemplate.execute(setEngineSql);
-    }
-    // public void resetAutoIncrement(String tableName) {
-    //     String resetSql = "TRUNCATE TABLE " + tableName;
-    //     jdbcTemplate.execute(resetSql);
-    // }
-}
+//         // Additional statement for InnoDB engine
+//         String setEngineSql = "ALTER TABLE " + tableName + " ENGINE=InnoDB";
+//         jdbcTemplate.execute(setEngineSql);
+//     }
+//     // public void resetAutoIncrement(String tableName) {
+//     //     String resetSql = "TRUNCATE TABLE " + tableName;
+//     //     jdbcTemplate.execute(resetSql);
+//     // }
+// }
--- src/main/java/th/mfu/domain/Game.java
+++ src/main/java/th/mfu/domain/Game.java
@@ -4,8 +4,10 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity 
+@Table(name = "GAME", schema = "new_schema")
 public class Game {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
