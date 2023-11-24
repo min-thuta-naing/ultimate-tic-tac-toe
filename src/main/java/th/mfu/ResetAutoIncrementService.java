1a28acdf377a369f961d3255b8a3d16de7c18b77
Min Thuta Naing <6531503157@lamduan.mfu.ac.th>
2023-11-24 18:50:05 +0700

    done

--- src/main/java/th/mfu/ResetAutoIncrementService.java
+++ src/main/java/th/mfu/ResetAutoIncrementService.java
@@ -10,12 +10,19 @@ public class ResetAutoIncrementService {
     private JdbcTemplate jdbcTemplate;
 
     public void resetAutoIncrement(String tableName) {
+
+        dropForeignKeyConstraint(tableName); 
         String resetSql = "ALTER TABLE " + tableName + " AUTO_INCREMENT = 1";
         jdbcTemplate.execute(resetSql);
 
         // Additional statement for InnoDB engine
         // String setEngineSql = "ALTER TABLE " + tableName + " ENGINE=InnoDB";
         // jdbcTemplate.execute(setEngineSql);
+    }
+
+    public void dropForeignKeyConstraint(String tableName){
+        String dropFK= "ALTER TABLE " + tableName + " DROP FOREIGN KEY FKk6liqsjt69vc76wx8hn8p52gm";
+        jdbcTemplate.execute(dropFK);
     }
     // public void resetAutoIncrement(String tableName) {
     //     String resetSql = "TRUNCATE TABLE " + tableName;
