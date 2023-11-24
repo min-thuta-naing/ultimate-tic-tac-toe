4af10813714f3f5486e66aea922c25ce4cba5fc2
Min Thuta Naing <6531503157@lamduan.mfu.ac.th>
2023-11-24 19:09:52 +0700

    ni

--- src/main/java/th/mfu/ResetAutoIncrementService.java
+++ src/main/java/th/mfu/ResetAutoIncrementService.java
@@ -21,12 +21,19 @@ public class ResetAutoIncrementService {
     }
 
     public void dropForeignKeyConstraint(String tableName) {
-        String checkForeignKeySql = "SELECT COUNT(*) FROM information_schema.table_constraints WHERE constraint_name = 'FKk6liqsjt69vc76wx8hn8p52gm'";
-        int count = jdbcTemplate.queryForObject(checkForeignKeySql, Integer.class);
+        // String checkForeignKeySql = "SELECT COUNT(*) FROM information_schema.table_constraints WHERE constraint_name = 'FKk6liqsjt69vc76wx8hn8p52gm'";
+        // int count = jdbcTemplate.queryForObject(checkForeignKeySql, Integer.class);
     
-        if (count > 0) {
-            String dropFK = "ALTER TABLE " + tableName + " DROP FOREIGN KEY FKk6liqsjt69vc76wx8hn8p52gm";
+        // if (count > 0) {
+        //     String dropFK = "ALTER TABLE " + tableName + " DROP FOREIGN KEY FKk6liqsjt69vc76wx8hn8p52gm";
+        //     jdbcTemplate.execute(dropFK);
+        // }
+        String foreignKeyName = "FKk6liqsjt69vc76wx8hn8p52gm";
+        String dropFK = "ALTER TABLE " + tableName + " DROP FOREIGN KEY " + foreignKeyName;
+        try {
             jdbcTemplate.execute(dropFK);
+        } catch (Exception e) {
+            e.printStackTrace(); // Log the exception
         }
     }
     
